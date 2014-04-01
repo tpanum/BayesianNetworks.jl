ab8efef7d44bebad65f0b6278d4b8e11d5cc3421
Esben Pilgaard Møller <epmo09@student.aau.dk>
2014-04-01 10:51:33 +0200

    tests for types

--- src/types.jl
+++ src/types.jl
@@ -25,33 +25,43 @@ function BayesianEdge(i::Int, s::BayesianNode, t::BayesianNode)
 end
 
 function add_node!(g::BayesianNetwork, n::BayesianNode)
-    @assert vertex_index(n) == num_vertices(g) + 1
+    @assert node_index(n) == num_nodes(g) + 1
     push!(g.nodes, n)
 end
 
 function add_edge!(g::BayesianNetwork, s::BayesianNode, t::BayesianNode)
     e = ExEdge(num_edges(g) + 1, s, t)
-    index = vertex_index(t)
+    index = node_index(t)
     push!(g.nodes[index].ingoingEdges, e)
     push!(g.edges, e)
 end
 
 function add_edge!(g::BayesianNetwork, e::ExEdge{BayesianNode})
     @assert edge_index(e) == num_edges(g) + 1
-    index = vertex_index(e.target)
+    index = node_index(e.target)
     push!(g.nodes[index].ingoingEdges, e)
     push!(g.edges, e)
 end
 
-vertex_index(n::BayesianNode) = n.index
+node_index(n::BayesianNode) = n.index
 
-num_vertices(g::BayesianNetwork) = length(g.nodes)
-vertices(g::BayesianNetwork) = g.nodes
+num_nodes(g::BayesianNetwork) = length(g.nodes)
+nodes(g::BayesianNetwork) = g.nodes
 
 num_edges(g::BayesianNetwork) = length(g.edges)
 edges(g::BayesianNetwork) = g.edges
 
 include("show.jl")
+
+b = BayesianNetwork(Array(BayesianNode,0), Array(ExEdge{BayesianNode},0))
+a1 = BayesianNode(1,1,Array(ExEdge{BayesianNode},0),ProbabilityDistribution([0.5,0.5],[1,2]))
+a2 = BayesianNode(2,1,Array(ExEdge{BayesianNode},0),ProbabilityDistribution([0.5,0.5],[1,2]))
+
+add_node!(b,a1)
+add_node!(b,a2)
+add_edge!(b,a1,a2)
+
+
 #a1 = BayesianNode(1,1,Array(ExEdge{BayesianNode},0),ProbabilityDistribution([0.5,0.5],[1,2])) 
 #a2 = BayesianNode(2,1,Array(ExEdge{BayesianNode},0),ProbabilityDistribution([0.5,0.5],[1,2])) 
 #e1 = BayesianEdge(1,a1,a2);
---
+++ test/typeTests.jl
@@ -0,0 +1,11 @@
+b = BayesianNetwork(Array(BayesianNode,0), Array(ExEdge{BayesianNode},0))
+a1 = BayesianNode(1,1,Array(ExEdge{BayesianNode},0),ProbabilityDistribution([0.5,0.5],[1,2]))
+a2 = BayesianNode(2,1,Array(ExEdge{BayesianNode},0),ProbabilityDistribution([0.5,0.5],[1,2]))
+
+add_node!(b,a1)
+add_node!(b,a2)
+add_edge!(b,a1,a2)
+
+@test num_edges(b) == 1
+@test num_nodes(b) == 2
+@test length(b.nodes[node_index(a2)].ingoingEdges) == 1
