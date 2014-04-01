14c1ff8892aff9d99b7981086f2cca13d419a623
Esben Pilgaard Møller <epmo09@student.aau.dk>
2014-04-01 10:05:45 +0200

    i think it works now

--- src/types.jl
+++ src/types.jl
@@ -1,26 +1,5 @@
 #using Graphs
-
-type ProbabilityDistribution{T <: FloatingPoint, K}
-    ps::Array{T,1}
-    states::Array{K,1}
-
-    function ProbabilityDistribution(_ps::Array{T,1}, _ss::Array{K,1})
-        if sum(_ps) != 1
-            throw("Probability Distributions must sum to 1")
-        end
-        new(_ps, _ss)
-    end
-end
-
-ProbabilityDistribution{T,K}(ps::Array{T,1}, ss::Array{K,1}) = ProbabilityDistribution{T,K}(ps, ss)
-
-function states(pd::ProbabilityDistribution)
-    pd.states
-end
-
-type CPT
-    PDs::Array{ProbabilityDistribution,1}
-end
+include("ProbabilityDistribution.jl")
 
 type BayesianNode
 	index::Int
