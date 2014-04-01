439a65fc7278947ea71642bd236af7f787799c08
Esben Pilgaard Møller <epmo09@student.aau.dk>
2014-04-01 11:13:24 +0200

    add-edge

--- test/typeTests.jl
+++ test/typeTests.jl
@@ -4,8 +4,8 @@ a2 = BayesianNode(2,1,Array(ExEdge{BayesianNode},0),ProbabilityDistribution([0.5
 
 add_node!(b,a1)
 add_node!(b,a2)
-#add_edge!(b,a1,a2)
+add_edge!(b,a1,a2)
 
-#@test num_edges(b) == 1
+@test num_edges(b) == 1
 @test num_nodes(b) == 2
-#@test length(b.nodes[node_index(a2)].ingoingEdges) == 1
+@test length(b.nodes[node_index(a2)].ingoingEdges) == 1
