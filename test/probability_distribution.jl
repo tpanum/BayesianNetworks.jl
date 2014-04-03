9f13d0e60d81f0738ea30e5c3d539ef5f6f28981
Esben Pilgaard Møller <epmo09@student.aau.dk>
2014-04-03 14:19:07 +0200

    Added additional testS

--- test/probability_distribution.jl
+++ test/probability_distribution.jl
@@ -10,3 +10,5 @@ pd2 = ProbabilityDistribution([0.5,0.5], ["tails","head"])
 pd3 = ProbabilityDistribution([0.6,0.4], ["tails","head"])
 
 @test_approx_eq 0.6 pd3["tails"]
+
+@test_throws pd3 = ProbabilityDistribution([0.7,0.4], ["tails","head"])
