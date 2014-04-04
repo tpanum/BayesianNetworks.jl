fb0b679ceebf6c3410a674d8b10a580d3b8158bd
bjarkehs <tixusdk@gmail.com>
2014-04-04 10:34:18 +0200

    Moving show function of CPD to show.jl. Third time's the charm

--- src/cpd.jl
+++ src/cpd.jl
@@ -8,19 +8,6 @@ type CPD
     CPD(bn1::Symbol, bn2::Symbol) = new([bn1],[bn2])
 end
 
-function Base.show(io::IO, cpd::CPD)
-    dist=map(bn -> string(bn), cpd.distribution)
-    j_dist=join(dist, ",")
-
-    if length(cpd.conditionals) > 0
-        cond=map(bn -> string(bn), cpd.conditionals)
-        j_cond=join(cond, ",")
-        print(io,"CPD{P($(j_dist)|$(j_cond))}")
-    else
-        print(io, "CPD{P($(j_dist))}")
-    end
-end
-
 |(bn1::Symbol, bn2::Symbol) = CPD(bn1,bn2)
 |(bn1::Array{Symbol,1}, bn2::Array{Symbol,1}) = CPD(bn1,bn2)
 |(bn1::Symbol, bn2::Array{Symbol,1}) = CPD(bn1,bn2)
