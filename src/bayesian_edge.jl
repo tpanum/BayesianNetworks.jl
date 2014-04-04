b374049ff337a35ba55122fda8a41a958434269e
bjarkehs <tixusdk@gmail.com>
2014-04-04 13:10:05 +0200

    Added comparison of BayesianEdge

--- src/bayesian_edge.jl
+++ src/bayesian_edge.jl
@@ -1,11 +1,15 @@
 import Graphs: edge_index, source, target
 
 type BayesianEdge
-	index::Int
-	source::BayesianNode
-	target::BayesianNode
+    index::Int
+    source::BayesianNode
+    target::BayesianNode
 
-	BayesianEdge(s::BayesianNode, t::BayesianNode) = new(0,s,t)
+    BayesianEdge(s::BayesianNode, t::BayesianNode) = new(0,s,t)
+end
+
+function ==(e1::BayesianEdge, e2::BayesianEdge)
+    e1.index == e2.index && e1.source == e2.source && e1.target == e2.target
 end
 
 BayesianEdge{T <: BayesianNode}(s::CBayesianNode, t::T) = throw("Source cannot be of type CBayesianNode")
--- test/bayesian_edge.jl
+++ test/bayesian_edge.jl
@@ -1,3 +1,6 @@
+pd1 = ProbabilityDistribution([0.5,0.5], ["head","tails"])
+pd2 = ProbabilityDistribution([0.3,0.7], ["head","tails"])
+
 a1 = DBayesianNode(:hulu, pd1)
 a2 = DBayesianNode(:bulu, pd2)
 b3 = CBayesianNode(:esben, x->x^0.5)
@@ -11,3 +14,29 @@ e2 = BayesianEdge(a1,b3)
 @test target(e1) == a2
 @test target(e2) == b3
 @test_throws e2 = BayesianEdge(b3,a1)
+
+k1 = DBayesianNode(:bjarke, pd1)
+k2 = DBayesianNode(:thomas, pd2)
+k3 = DBayesianNode(:esben, pd1)
+
+h1 = BayesianEdge(k1,k2)
+h2 = BayesianEdge(k1,k2)
+h3 = BayesianEdge(k2,k1)
+h4 = BayesianEdge(k1,k3)
+
+@test h1 == h1
+@test h1 == h2
+@test h1 != h3
+@test h3 != h4
+@test h1 != h4
+
+h1.index = 4
+h2.index = 3
+h3.index = 2
+h4.index = 1
+
+@test h1 == h1
+@test h1 != h2
+@test h1 != h3
+@test h3 != h4
+@test h1 != h4
