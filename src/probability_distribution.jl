d99f86f765a754b828547e2a8ced1ce724da1344
tpanum <thomas@panum.dk>
2014-04-07 12:44:47 +0200

    ProbabilityDistributions are now able to be 2-dimensional

--- src/probability_distribution.jl
+++ src/probability_distribution.jl
@@ -1,23 +1,41 @@
 import Base.length
 
-type ProbabilityDistribution{T <: FloatingPoint}
-    ps::Array{T,1}
-    states::Array{ASCIIString,1}
+type ProbabilityDistribution{T <: Real,K}
+    ps::Array{T}
+    dim_states::(Array{K,1},Array{K,1})
 
-    function ProbabilityDistribution(_ps::Array{T,1}, _ss::Array{ASCIIString,1})
+    function check_pd_sum!{N <: Real}(_ps::Array{N})
         if sum(_ps) != 1
             throw("Probability Distributions must sum to 1")
+        else
+            true
         end
+    end
+    
+    function ProbabilityDistribution(_ps::Array{T,1}, _ss::Array{K,1})
+
+        check_pd_sum!(_ps)
         order=sortperm(_ss)
-        new(_ps[order], _ss[order])
+        new(_ps[order], (_ss[order],))
+    end
+    function ProbabilityDistribution(_ps::Array{T,2}, _ss::Array{Array{K,1},1})
+        check_pd_sum!(_ps)
+
+        if length(_ss) != 2
+            throw("Dimension mismatch")
+        end
+        
+        order_dim_1=sortperm(_ss[1])
+        order_dim_2=sortperm(_ss[2])
+        new(_ps[order_dim_1,order_dim_2], (_ss[1][order_dim_1],_ss[2][order_dim_1]))
     end
 end
 
-ProbabilityDistribution{T}(ps::Array{T,1}, ss::Array{ASCIIString,1}) = ProbabilityDistribution{T}(ps, ss)
+ProbabilityDistribution{T,K}(ps::Array{T,1}, ss::Array{K,1}) = ProbabilityDistribution{T,K}(ps, ss)
+ProbabilityDistribution{T,K}(ps::Array{T,2}, ss::Array{Array{K,1},1}) = ProbabilityDistribution{T,K}(ps, ss)
 
-function states(pd::ProbabilityDistribution)
-    pd.states
-end
+states(pd::ProbabilityDistribution) = pd.dim_states
+states(pd::ProbabilityDistribution,i::Integer) = states(pd)[i]
 
 function probabilities(pd::ProbabilityDistribution)
     pd.ps
@@ -27,11 +45,23 @@ function length(pd::ProbabilityDistribution)
     length(pd.ps)
 end
 
-function getindex{T,K}(pd::ProbabilityDistribution{T}, key::K)
-    if !(key in pd.states)
+function getindex{T,K}(pd::ProbabilityDistribution{T,K}, key::K)
+    if !(key in states(pd)[1])
         throw("Invalid key")
     end
-    pd.ps[key .== pd.states]
+    pd.ps[key .== states(pd)[1]]
+end
+
+function getindex{T,K}(pd::ProbabilityDistribution{T,K}, key1::K, key2::K)
+    if !(key1 in states(pd)[1])
+        throw("Invalid key")
+    end
+
+    if !(key2 in states(pd)[2])
+        throw("Invalid key")
+    end
+    
+    pd.ps[key1 .== states(pd)[1], key2 .== states(pd)[2]][1]
 end
 
 function ==(pd1::ProbabilityDistribution, pd2::ProbabilityDistribution)
@@ -44,4 +74,3 @@ function lpd{K}(states::K)
     fill!(pb, 1/m)
     ProbabilityDistribution(pb, states)
 end
-
--- test/probability_distribution.jl
+++ test/probability_distribution.jl
@@ -1,6 +1,6 @@
 pd1 = ProbabilityDistribution([0.5,0.5], ["head","tails"])
 
-@test states(pd1) == ["head","tails"]
+@test states(pd1) == (["head","tails"],)
 @test_approx_eq 0.5 pd1["head"]
 
 pd2 = ProbabilityDistribution([0.5,0.5], ["tails","head"])
@@ -12,3 +12,11 @@ pd3 = ProbabilityDistribution([0.6,0.4], ["tails","head"])
 @test_approx_eq 0.6 pd3["tails"]
 
 @test_throws pd3 = ProbabilityDistribution([0.7,0.4], ["tails","head"])
+
+
+# 2-dimensional
+
+pd2_1 = ProbabilityDistribution([0.4 0.1; 0.25 0.25], Array{ASCIIString,1}[["tails","head"],["happy","sad"]])
+
+#@test pd2_1["tails","happy"] == 0.4
+
