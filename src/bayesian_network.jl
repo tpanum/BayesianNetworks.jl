778c20c6f869efa4269d8aca0ced578cfec8e4ff
bjarkehs <tixusdk@gmail.com>
2014-04-08 11:22:45 +0200

    Removed an unused variable

--- src/bayesian_network.jl
+++ src/bayesian_network.jl
@@ -213,7 +213,6 @@ function check_requirements(g::BayesianNetwork, cpd::CPD)
     conds = conditionals(cpd)
     dist = distribution(cpd)
     to_check = [P(conds|dist), P(conds), P(dist)]
-    the_check
     for i in to_check
         if check_requirement(g, i) == false
             return false
