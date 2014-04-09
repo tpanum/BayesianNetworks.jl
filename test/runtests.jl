03ecae1fd00e6f24faa82c21a3abc01dd6161dcd
tpanum <thomas@panum.dk>
2014-04-09 15:25:56 +0200

    Added pretty print to test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,17 @@ using Graphs
 using BayesianNetworks
 using Base.Test
 
-include("utility.jl")
-include("probability_distribution.jl")
-include("cpd.jl")
-include("bayesian_node.jl")
-include("bayesian_network.jl")
-include("bayesian_edge.jl")
-include("probability_density_distribution.jl")
-include("diagnosis_test.jl")
+function add_test_package(file::ASCIIString)
+    start_time=time()
+    include(file)
+    delta_time=round(time()-start_time,2)
+    println("[\u2713] $file ($(delta_time)s)")
+end
+add_test_package("utility.jl")
+add_test_package("probability_distribution.jl")
+add_test_package("cpd.jl")
+add_test_package("bayesian_node.jl")
+add_test_package("bayesian_network.jl")
+add_test_package("bayesian_edge.jl")
+add_test_package("probability_density_distribution.jl")
+add_test_package("diagnosis_test.jl")
